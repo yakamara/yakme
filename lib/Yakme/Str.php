f716235ee254107029975bf767a606ca26c8b63a
tbaddade <thomas@redaxo.org>
2018-09-19 13:56:09 +0200

    Str helper class

---
+++ lib/Yakme/Str.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Yakme;
+
+class Str
+{
+    /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    public static function startsWith($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
